adfa8db476517e0e31f477f2b1bcd8fc90571d78
christopher-t-smith <christophert.smith2016@gmail.com>
2023-03-15 17:51:48 -0500

    Implemented welcome and new game option, AB#148

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package com.escapeartist.views;
+
+import com.escapeartist.models.FlashMenuScreen;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        FlashMenuScreen flashmenu = new FlashMenuScreen();
+
+    }
+}
