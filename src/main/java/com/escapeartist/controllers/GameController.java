ac37d6bd506dab2f20931a7d5c2a790f15522b6e
hussain-ghani <hussain.ghani@tlglcohort.com>
2023-03-17 16:21:24 -0400

    fixed nullpointerexception, AB#151

--- src/main/java/com/escapeartist/controllers/GameController.java
+++ src/main/java/com/escapeartist/controllers/GameController.java
@@ -1,25 +1,27 @@
 package com.escapeartist.controllers;
 
-import com.escapeartist.models.TextParser;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import com.escapeartist.models.TextParser;
 
 public class GameController {
+
     private JsonObject gameData;
-    private final TextParser parser;
+    private TextParser textParser;
 
     public GameController() {
-        parser = new TextParser(gameData);
         loadGameData();
+        textParser = new TextParser(gameData);
     }
 
     private void loadGameData() {
         Gson gson = new Gson();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("game_dialogue.json");
+        InputStream inputStream = getClass().getClassLoader()
+            .getResourceAsStream("game_dialogue.json");
         gameData = gson.fromJson(new InputStreamReader(inputStream), JsonObject.class);
     }
 
@@ -30,49 +32,24 @@ public class GameController {
         while (running) {
             System.out.print(gameData.get("command_prompt").getAsString());
             String userInput = scanner.nextLine();
-            JsonElement inputElement = parser.cleanInput(userInput);
+            String cleanedInput = textParser.cleanUserInput(userInput);
+            JsonElement inputElement = new Gson().toJsonTree(cleanedInput);
 
-            JsonObject validInputs = gameData.getAsJsonObject("valid_inputs");
+            if (textParser.isQuitCommand(inputElement)) {
+                boolean confirmQuit = textParser.getConfirmation(
+                    gameData.get("quit_confirm").getAsString());
 
-            if (parser.handleQuit(validInputs, inputElement, gameData.get("quit_confirm").getAsString(), gameData.get("command_prompt").getAsString(), gameData.get("invalid_input").getAsString(), scanner)) {
-                System.out.println(gameData.get("goodbye_message").getAsString());
-                running = false;
-
+                if (confirmQuit) {
+                    System.out.println(gameData.get("goodbye_message").getAsString());
+                    running = false;
+                }
             } else {
-                if (!parser.isValidInput(validInputs, inputElement)) {
+                if (!textParser.isValidInput(inputElement)) {
                     System.out.println(gameData.get("invalid_input").getAsString());
                 }
-            }
-            scanner = new Scanner(System.in);
-        }
-    }
 
-    private boolean getConfirmation(String prompt) {
-        Scanner scanner = new Scanner(System.in);
-        JsonObject validInputs = gameData.getAsJsonObject("valid_inputs");
-
-        while (true) {
-            System.out.println(prompt);
-            System.out.print(gameData.get("command_prompt").getAsString());
-            String userInput = scanner.nextLine().trim().toLowerCase();
-            JsonElement inputElement = new Gson().toJsonTree(userInput);
-
-            if (validInputs.getAsJsonArray("yes").contains(inputElement)) {
-                return true;
-            } else if (validInputs.getAsJsonArray("no").contains(inputElement)) {
-                return false;
-            } else {
-                System.out.println(gameData.get("invalid_input").getAsString());
+                // TODO: Add game logic here
             }
         }
-    }
-
-    private boolean isValidInput(JsonObject validInputs, JsonElement inputElement) {
-        for (String key : validInputs.keySet()) {
-            if (validInputs.getAsJsonArray(key).contains(inputElement)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
