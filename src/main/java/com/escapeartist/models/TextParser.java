83bc2b2693d8944bc472a65bc90b9f094b91cc9d
hussain-ghani <hussain.ghani@tlglcohort.com>
2023-03-17 16:21:24 -0400

    fixed nullpointerexception, merged changes, AB#151, AB#157

--- src/main/java/com/escapeartist/models/TextParser.java
+++ src/main/java/com/escapeartist/models/TextParser.java
@@ -4,18 +4,30 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
 import java.util.Scanner;
 
 public class TextParser {
-
-  private final JsonObject gameData;
+  private JsonObject gameData;
 
   public TextParser(JsonObject gameData) {
     this.gameData = gameData;
   }
 
+  public String cleanUserInput(String userInput) {
+    userInput = userInput.trim().toLowerCase();
+    JsonArray wordsToRemove = gameData.getAsJsonArray("words_to_remove");
 
-  public boolean isValidInput(JsonObject validInputs, JsonElement inputElement) {
+    for (JsonElement wordToRemove : wordsToRemove) {
+      String word = wordToRemove.getAsString();
+      userInput = userInput.replaceAll("\\b" + word + "\\b", "").trim();
+    }
+
+    return userInput;
+  }
+
+  public boolean isValidInput(JsonElement inputElement) {
+    JsonObject validInputs = gameData.getAsJsonObject("valid_inputs");
     for (String key : validInputs.keySet()) {
       if (validInputs.getAsJsonArray(key).contains(inputElement)) {
         return true;
@@ -24,34 +36,28 @@ public class TextParser {
     return false;
   }
 
-  public JsonElement cleanInput(String userInput) {
-    userInput = userInput.trim().toLowerCase();
-    JsonArray wordsToRemove = gameData.getAsJsonArray("words_to_remove");
-    for (JsonElement wordToRemove : wordsToRemove) {
-      userInput = userInput.replaceAll("\\b" + wordToRemove.getAsString() + "\\b", "").trim();
-    }
-    return new Gson().toJsonTree(userInput);
+  public boolean isQuitCommand(JsonElement inputElement) {
+    JsonObject validInputs = gameData.getAsJsonObject("valid_inputs");
+    return validInputs.getAsJsonArray("quit").contains(inputElement);
   }
 
-  public boolean handleQuit(JsonObject validInput, JsonElement inputElement,
-      String quitConfirmMessage, String commandPrompt, String invalidInputMessage, Scanner scanner) {
-    if (validInput.getAsJsonArray("quit").contains(inputElement)) {
-      System.out.println(quitConfirmMessage);
-      System.out.print(commandPrompt);
+  public boolean getConfirmation(String prompt) {
+    Scanner scanner = new Scanner(System.in);
+    JsonObject validInputs = gameData.getAsJsonObject("valid_inputs");
+
+    while (true) {
+      System.out.println(prompt);
+      System.out.print(gameData.get("command_prompt").getAsString());
       String userInput = scanner.nextLine().trim().toLowerCase();
-      JsonElement confirmInput = cleanInput(userInput);
-      if (validInput.getAsJsonArray("yes").contains(confirmInput)) {
+      JsonElement inputElement = new Gson().toJsonTree(userInput);
+
+      if (validInputs.getAsJsonArray("yes").contains(inputElement)) {
         return true;
-      } else if (validInput.getAsJsonArray("no").contains(confirmInput)) {
+      } else if (validInputs.getAsJsonArray("no").contains(inputElement)) {
         return false;
       } else {
-        System.out.println(gameData.get("invalidInputMessage").getAsString());
-        return false;
+        System.out.println(gameData.get("invalid_input").getAsString());
       }
     }
-    return false;
   }
 }
-
-
-
